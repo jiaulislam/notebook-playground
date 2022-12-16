bba69a29b345da953e346ac021b0cae2808cad1b
jiaulislam <kmjiaulislam@gmail.com>
2022-12-16 14:28:36 +0600

    learned sqlalchemy many to many relationship setup

--- myapp/models.py
+++ myapp/models.py
@@ -3,9 +3,20 @@ import sqlalchemy as sa
 from sqlalchemy.inspection import inspect
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
+
+MAX_INCREMENT_VALUE = 999999999999999999999999999
+
 # Declare the Declrative base
 class Base(DeclarativeBase):
-    pass
+    def __repr__(self) -> str:
+        return (
+            f"{self.__class__.__name__!s}("
+            + ",".join(
+                str(key) + "=" + f"{vars(self).get(key, None)!r}"
+                for key in inspect(self.__class__).columns.keys()  # type: ignore[union-attr]
+            )
+            + ")"
+        )
 
 
 # Customer ORM Model of SQLAlchemy
@@ -13,25 +24,17 @@ class CustomerOrm(Base):
     __tablename__ = "customers"
 
     id: Mapped[int] = mapped_column(
-        sa.Identity(start=1, cycle=True, maxvalue=999999999999999999999999999),
+        sa.Identity(start=1, cycle=True, maxvalue=MAX_INCREMENT_VALUE),
         primary_key=True,
     )
-    name: Mapped[str] = mapped_column(sa.String(30))
-    contact_number: Mapped[str] = mapped_column(sa.String(60))
-    is_active: Mapped[bool] = mapped_column(default=True)
+    name: Mapped[str] = mapped_column(sa.String(30), nullable=False)
+    contact_number: Mapped[str] = mapped_column(sa.String(60), nullable=False)
+    is_active: Mapped[bool] = mapped_column(default=True, nullable=False)
 
     # Nullable Reference Required ?
     addresses: Mapped[Optional["AddressOrm"]] = relationship(back_populates="customer")
 
-    def __repr__(self) -> str:
-        return (
-            f"CustomerOrm("
-            + ",".join(
-                str(key) + "=" + f"{vars(self).get(key)!r}"
-                for key in inspect(CustomerOrm).columns.keys()
-            )
-            + ")"
-        )
+    # orders: Mapped[Optional[list["OrderOrm"]]] = relationship(back_populates="customer")
 
 
 # Customer Address Model of SQLAlchemy
@@ -39,21 +42,120 @@ class AddressOrm(Base):
     __tablename__ = "addresses"
 
     id: Mapped[int] = mapped_column(
-        sa.Identity(start=1, cycle=True, maxvalue=999999999999999999999999999),
+        sa.Identity(start=1, cycle=True, maxvalue=MAX_INCREMENT_VALUE),
         primary_key=True,
     )
     customer_id: Mapped[int] = mapped_column(sa.ForeignKey("customers.id"))
-    present_address: Mapped[str] = mapped_column(sa.String(255))
+    present_address: Mapped[str] = mapped_column(sa.String(255), nullable=False)
     permenent_address: Mapped[str] = mapped_column(sa.String(255))
 
     customer: Mapped["CustomerOrm"] = relationship(back_populates="addresses")
 
-    def __repr__(self) -> str:
-        return (
-            f"AddressOrm("
-            + ",".join(
-                str(key) + "=" + f"{vars(self).get(key)!r}"
-                for key in inspect(AddressOrm).columns.keys()
-            )
-            + ")"
-        )
+
+# As per the documentation `The association table is nearly always given as a Core Table object or
+# another Core selecteable such as Join object, and is indicated by relationship.secondary argument to relationship().
+# Usual, the Table uses the MetaData  object associated with the declarative base class, so that the ForeignKey directives
+# can locate the remote tables with thich to link`
+# refference: https://docs.sqlalchemy.org/en/20/orm/basic_relationships.html#relationships-many-to-many
+
+# Note: for a core table, we use the sqlalchemy.Column construct, not sqlalchemy.orm.mapped_column
+product_tag_assoc_tbl = sa.Table(
+    "product_tag_assoc",
+    Base.metadata,
+    sa.Column(
+        "id",
+        sa.Integer,
+        sa.Identity(start=1, maxvalue=MAX_INCREMENT_VALUE, cycle=True),
+        primary_key=True,
+    ),
+    sa.Column("product_id", sa.ForeignKey("products.id"), nullable=False),
+    sa.Column("tag_id", sa.ForeignKey("tags.id"), nullable=False),
+)
+
+product_category_assoc_tbl = sa.Table(
+    "product_category_assoc",
+    Base.metadata,
+    sa.Column(
+        "id",
+        sa.Integer,
+        sa.Identity(start=1, maxvalue=MAX_INCREMENT_VALUE, cycle=True),
+        primary_key=True,
+    ),
+    sa.Column("product_id", sa.ForeignKey("products.id"), nullable=False),
+    sa.Column("category_id", sa.ForeignKey("categories.id"), nullable=False),
+)
+
+
+class TagOrm(Base):
+    __tablename__ = "tags"
+
+    id: Mapped[int] = mapped_column(
+        sa.Identity(start=1, maxvalue=MAX_INCREMENT_VALUE, cycle=True), primary_key=True
+    )
+    name: Mapped[str] = mapped_column(sa.String(30))
+
+    products: Mapped[Optional[set["ProductOrm"]]] = relationship(
+        secondary=product_tag_assoc_tbl, back_populates="tags"
+    )
+
+
+class CategoryOrm(Base):
+    __tablename__ = "categories"
+
+    id: Mapped[int] = mapped_column(
+        sa.Identity(start=1, maxvalue=MAX_INCREMENT_VALUE, cycle=True), primary_key=True
+    )
+    title: Mapped[str] = mapped_column(sa.String(30), nullable=False)
+
+    products: Mapped[Optional[set["ProductOrm"]]] = relationship(
+        secondary=product_category_assoc_tbl, back_populates="categories"
+    )
+
+
+# Producs Model of SQLAlchemy
+class ProductOrm(Base):
+    __tablename__ = "products"
+
+    id: Mapped[int] = mapped_column(
+        sa.Identity(start=1, maxvalue=MAX_INCREMENT_VALUE, cycle=True), primary_key=True
+    )
+    code: Mapped[str] = mapped_column(sa.String(30))
+    name: Mapped[str] = mapped_column(sa.String(80))
+    tags: Mapped[Optional[set["TagOrm"]]] = relationship(
+        secondary=product_tag_assoc_tbl, back_populates="products"
+    )
+
+    categories: Mapped[Optional[set["CategoryOrm"]]] = relationship(
+        secondary=product_category_assoc_tbl, back_populates="products"
+    )
+
+
+# class ProductCategoryOrm(Base):
+#     __tablename__ = "products_categories_assoc"
+#     id: Mapped[int] = mapped_column(
+#         sa.Identity(start=1, nomaxvalue=True), primary_key=False
+#     )
+#     product_id: Mapped[int] = mapped_column(
+#         sa.ForeignKey("products.id"), nullable=False
+#     )
+#     category_id: Mapped[int] = mapped_column(
+#         sa.ForeignKey("categories.id"), nullable=False
+#     )
+
+
+# #  Customer Order Model of SQLAlchemy
+# class OrderOrm(Base):
+#     __tablename__ = "orderes"
+
+#     id: Mapped[int] = mapped_column(
+#         sa.Identity(start=1, nomaxvalue=True), primary_key=True
+#     )
+#     order_no: Mapped[str] = mapped_column(sa.String(30), nullable=False)
+#     customer_id: Mapped["CustomerOrm"] = mapped_column(
+#         sa.ForeignKey("customers.id"), nullable=False
+#     )
+#     product_id: Mapped["ProductOrm"] = mapped_column(
+#         sa.ForeignKey("products.id"), nullable=False
+#     )
+
+#     customer: Mapped["CustomerOrm"] = relationship(back_populates="orders")
